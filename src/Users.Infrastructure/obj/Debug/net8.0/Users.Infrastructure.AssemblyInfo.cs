ccf13f7796975dc9415d5374cd1487e00ab9581c
Joao Eduardo Correa <joao.eduardu@gmail.com>
2026-07-14 23:47:24 -0300

    ++

--- src/Users.Infrastructure/obj/Debug/net8.0/Users.Infrastructure.AssemblyInfo.cs
+++ src/Users.Infrastructure/obj/Debug/net8.0/Users.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Users.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da2c455cc1cba403d78ded98902c9dd31da5374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a97724c4aeae7a68781b49576c7befa0d3d64ae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Users.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Users.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
